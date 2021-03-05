5a3895dd94f1656a767567106c38ed1ef4f931e3
Olga MaciaszekSharma <olga.maciaszek@gmail.com>
2021-03-05 16:13:35 +0100

    Refactor.

--- card-service/src/main/java/io/github/olgamaciaszek/cardservice/config/RestTemplateConfig.java
+++ card-service/src/main/java/io/github/olgamaciaszek/cardservice/config/RestTemplateConfig.java
@@ -1,10 +1,7 @@
 package io.github.olgamaciaszek.cardservice.config;
 
-import io.github.olgamaciaszek.excluded.CustomLoadBalancerConfiguration;
-
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -13,7 +10,7 @@ import org.springframework.web.client.RestTemplate;
  * @author Olga Maciaszek-Sharma
  */
 @Configuration
-@LoadBalancerClient(value = "fraud-verifier", configuration = CustomLoadBalancerConfiguration.class)
+//@LoadBalancerClient(value = "fraud-verifier", configuration = CustomLoadBalancerConfiguration.class)
 public class RestTemplateConfig {
 
 	@Bean
